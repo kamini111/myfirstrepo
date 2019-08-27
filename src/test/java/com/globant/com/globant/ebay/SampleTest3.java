4f167b04d0f8580aab44936f3dd9790fded56ae3
Kamini Jain <kamini.jain@globant.com>
2019-08-27 15:50:28 +0530

    Second commit

---
+++ src/test/java/com/globant/com/globant/ebay/SampleTest3.java
@@ -0,0 +1,12 @@
+package com.globant.com.globant.ebay;
+
+import org.testng.annotations.Test;
+
+public class SampleTest3 {
+	@Test
+	public void TestA()
+	{
+		System.out.println("In Test A");
+	}
+
+}
